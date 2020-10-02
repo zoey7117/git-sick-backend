0c5dd6b7c924cfaef124618c5056f23f30768df9
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-10-02 19:01:46 -0400

    update

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,31 @@
-const Mutations = {};
+const Mutations = {
+	async createItem(parent, args, ctx, info) {
+		// @TODO: Check if they are logged in
+
+		const item = await ctx.db.mutation.createItem(
+			{
+				data: {
+					...args
+				}
+			},
+			info
+		);
+
+		return item;
+	},
+	updateItem(parent, args, ctx, info) {
+		const updates = { ...args };
+		delete updates.id;
+		return ctx.db.mutation.updateItem(
+			{
+				data: updates,
+				where: {
+					id: args.id
+				}
+			},
+			info
+		);
+	}
+};
 
 module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,15 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+	items: forwardTo('db'),
+	item: forwardTo('db')
+
+	// items(parent, args, ctx, info) {
+	//   return ctx.db.query.items();
+	// }
+	// dogs(parent, args, ctx, info) {
+	//   return [{ name: "snickers" }, { name: "lola" }];
+	// }
+};
 
 module.exports = Query;
