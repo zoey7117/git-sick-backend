1262bb8de018f257e2f054d8635303c82b1ff109
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-10-08 15:57:57 -0400

    created signup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 // let's go!
-require("dotenv").config({ path: ".env" });
-const createServer = require("./createServer");
-const db = require("./db");
+
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config({ path: '.env' });
+const createServer = require('./createServer');
+const db = require('./db');
 
 const server = createServer();
+server.express.use(cookieParser());
 
 server.start(
-  {
-    cors: {
-      credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
-  },
-  deets => {
-    console.log(`Server is now running on port http://localhost:${deets.port}`);
-  }
+	{
+		cors: {
+			credentials: true,
+			origin: process.env.FRONTEND_URL
+		}
+	},
+	(deets) => {
+		console.log(`Server is now running on port http://localhost:${deets.port}`);
+	}
 );
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
 	async createItem(parent, args, ctx, info) {
 		// @TODO: Check if they are logged in
@@ -32,6 +35,31 @@ const Mutations = {
 		const item = await ctx.db.query.item({ where }, `{ id title}`);
 		//3. delete the item
 		return ctx.db.mutation.deleteItem({ where }, info);
+	},
+	async signup(parent, args, ctx, info) {
+		args.email = args.email.toLowerCase();
+		//hash their passsword
+		const password = await bcrypt.hash(args.password, 10);
+		//create user in database
+		const user = await ctx.db.mutation.createUser(
+			{
+				data: {
+					...args,
+					password,
+					permissions: { set: [ 'USER' ] }
+				}
+			},
+			info
+		);
+		//create jwt token
+		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+		//set jwt as a cookie on the response
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365 //one year cookie
+		});
+		//return user to the browser
+		return user;
 	}
 };
 
